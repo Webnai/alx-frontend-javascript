4e020486e0e847923900af2a0d83745fae254b48
Wilbert Boadzo Elekplim <wilbertboadzo144@gmail.com>
2023-09-29 00:09:44 +0000

    sky high

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,31 +2,22 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    // Validate input parameters
-    if (typeof sqft !== 'number' || typeof floors !== 'number') {
-      throw new TypeError('Square footage and floors must be numbers');
-    }
-
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  // Setter for floors
   set floors(newFloors) {
     if (typeof newFloors !== 'number') {
-      throw new TypeError('Floors must be a number');
+      throw TypeError('Floors must be a number');
     }
     this._floors = newFloors;
   }
 
-  // Add a comment explaining the purpose of this method
   evacuationWarningMessage() {
-    return `In case of emergency, evacuate the ${this._floors} floors gradually.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-
