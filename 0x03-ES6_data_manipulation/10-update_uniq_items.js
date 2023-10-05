0d7960eb66d569a5ea3d46d06bb45dd7d8bc030d
Wilbert Boadzo Elekplim <wilbertboadzo144@gmail.com>
2023-10-05 01:01:11 +0000

    update uniq items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,6 @@
-export default function updateUniqueItems(items) {
-  if (!(items instanceof Map)) {
-    throw new Error('Cannot process');
-  }
-  for (const [key, value] of items) {
-    if (value === 1) {
-      items.set(key, 100);
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const i of map) if (i[1] === 1) map.set(i[0], 100);
+  return map;
+}
+
